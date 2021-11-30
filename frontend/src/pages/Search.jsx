6d6ecf7fc4206e81dee04c71d016145799d57956
Luke Swallow <luke.swallow334@gmail.com>
2021-11-29 23:58:55 -0800

    added search on agolia

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -2,24 +2,20 @@ import { InputGroup, FormControl, Button } from 'react-bootstrap';
 import SearchResults from '../components/SearchResults';
 import "../styles/Search.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import algoliasearch from 'algoliasearch';
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
 
+const searchClient = algoliasearch('HDOSHNNRSV', 'fe5ffe6f93cfcd34b6269a5cac5bf24f');
 function SearchAgreements(){
 
-
   return(
-  <div className="center">
-    <InputGroup className="search-bar">
-      <FormControl
-        placeholder="Company Name"
-        aria-describedby="basic-addon2"
-      />
-      <Button className="search-button" id="button-addon2">
-      Button
-    </Button>
-    </InputGroup>
-    <SearchResults />
-
-  </div>
+    <div className="center">
+    <InstantSearch searchClient={searchClient} indexName="Agreevio">
+      <SearchBox />
+      <Hits />
+    </InstantSearch>
+    </div>
   );
+
 }
 export default SearchAgreements;
