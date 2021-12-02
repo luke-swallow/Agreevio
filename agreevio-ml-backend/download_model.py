9d3097d373d4d3bfcd265b98fc980124125fb110
Matthew <mnemeth6@berkeley.edu>
2021-12-01 19:38:36 -0800

    update download_model

--- agreevio-ml-backend/download_model.py
+++ agreevio-ml-backend/download_model.py
@@ -1,12 +1,14 @@
-import gdown
-import shutil
+from gdown import download
+from shutil import unpack_archive
 import os
 
 def download_model():
-	url = "https://drive.google.com/u/1/uc?export=download&id=1UvYnEzjaRn3CVwetLgN4GWvHqZuRJjFO"
+	url = "https://drive.google.com/u/1/uc?export=download&id=1JZ7pYrkV_xatXh-0R3eB4DFFyw4DKauP"
 	filename = 't5-small.zip'
-	gdown.download(url, filename, quiet=False)
-	shutil.unpack_archive(filename, 't5-small')
+	download(url, filename, quiet=False)
+	if os.path.isdir('models') is False:
+		os.mkdir('models')
+	unpack_archive(filename, 'models/t5-small')
 	os.remove(filename)
 
 if __name__ == "__main__":
